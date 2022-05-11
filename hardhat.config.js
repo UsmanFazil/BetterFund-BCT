2f7a394ee2d6a706dd96bdfbcf216bf76b84d93b
Alex Lazar <alex2012ro@gmail.com>
2022-05-11 03:50:08 +0300

    fix: change block number

--- hardhat.config.js
+++ hardhat.config.js
@@ -42,7 +42,7 @@ module.exports = {
         url:
           process.env.POLYGON_RPC_CLIENT_URL ||
           "https://matic-mainnet.chainstacklabs.com",
-        blockNumber: 26276654,
+        blockNumber: 28171596,
         accounts: [
           process.env.PRIVATE_KEY ||
             "0dc178b655bb48f3478ebd42db1d40d9193d0ec96357ebd9ed0a5a165f5841cf",
